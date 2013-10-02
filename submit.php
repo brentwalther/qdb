713aa57345ebd9f18072c802bf9cfb0b95e4d1d5
Brent Walther <brentwalther@gmail.com>
2013-10-01 23:01:47 -0500

    Removed useless comments. Added a simple XSS prevention mechanism.

--- submit.php
+++ submit.php
@@ -5,19 +5,11 @@
 
     R::setup('mysql:host=localhost;dbname=brentwal_db1', $dbu, $dbp);
 
-	//Ready. Now insert a bean!
-	//$user = R::dispense('user');
-	//$user->username = "brentwalther";
-	//$user->email = "brent@walther.io";
-
-	//R::store($user, 'user');
-
 	$user = R::load('users', $_SESSION['id']);
 
-	//echo $user->username ." ". $user->email ."\n";
 	if(isset($_SESSION['id']) && isset($_POST['quoteText'])) {
 		$post = R::dispense('post');
-		$post->text = $_POST['quoteText'];
+		$post->text = htmlspecialchars($_POST['quoteText']);
 		$post->stamp = date("F jS, Y,  H:ia T");
 		$post->author = $user->id;
 		$post->anon = (isset($_POST['anon']) ? "yes" : "no");
